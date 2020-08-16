a91428f68dfd8cc8311f5cc2a5e9a9e409bf5ebb
richieok <richie.okoro@gmail.com>
2020-08-16 17:37:03 +0100

    stable. testing cookies

--- app.js
+++ app.js
@@ -1,59 +1,48 @@
 const express = require('express');
-const {init} = require('dbase');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const { init } = require('dbase');
 require('dotenv').config();
 
 const { PORT } = process.env;
 const port = PORT || 5505;
 
-// const { client, db } = init();
-// const db = dbase.getDb();
-// const client = dbase.getClient();
-// console.log('\nclient');
-// console.log(client);
-// console.log('\n\ndb');
-// console.log(db);
-
 const index = require('./routes/index');
 const verify = require('./routes/verify');
 
 const app = express();
+app.use(cookieSession({
+    name: 'session',
+    keys: ['apples', 'oranges'],
+    maxAge: 600000,
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/', index);
 app.use('/verify', verify);
 
-init().then( result => {
-    if (result.db && result.client){
+init().then(result => {
+    if (result.db && result.client) {
         const server = app.listen(port, () => console.log(`Backend is listening on port ${PORT}`));
-        return { db: result.db, client: result.client, server};
+        return { db: result.db, client: result.client, server };
     }
-}).then( result => {
-    process.on('SIGINT', async ()=>{
-        try{
+    throw result;
+}).then(result => {
+    process.on('SIGINT', async () => {
+        try {
             console.log('SIGINT RECEIVED');
             result.server.close();
+            console.log(`Server stopped listening on port ${port}`);
             result.client.close();
-        } catch (e){
+            console.log('Database connection closed');
+        } catch (e) {
             console.log(e);
         } finally {
             process.exit(0);
         }
     });
-})
-.catch( error => console.log(error));
-
-
-// process.on('SIGINT', async () => {
-//     try {
-//         console.log('SIGINT RECEIVED');
-//         console.log('closing server');
-//         server.close();
-//     } catch ( e ){
-//         console.log(e);
-//     } finally {
-//         process.exit(0);
-//     }
-//     // if (client){
-//     //     await client.close();
-//     //     console.log('db closed');
-//     // }
-// });
+}).catch(error => {
+    console.log(error.name);
+    console.log(error.message);
+});
--- routes/verify.js
+++ routes/verify.js
@@ -1,8 +1,10 @@
 const Router = require('express').Router();
 
 Router.get('/', (req, res)=>{
+    req.session.visits = ( req.session.visits || 0) + 1;
     res.json({
-        path: 'verify'
+        path: 'verify',
+        visits: req.session.visits
     });
 });
 
